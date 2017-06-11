0af80fb42350b8d0bf81cd2bfb28abf4f4aed970
Fahad Mutair <kelezq8@outlook.com>
2017-06-12 01:28:41 +0700

    hourly activity

--- app/src/main/java/com/teamtreehouse/stormy/ui/HourlyForecastActivity.java
+++ app/src/main/java/com/teamtreehouse/stormy/ui/HourlyForecastActivity.java
@@ -1,15 +1,26 @@
 package com.teamtreehouse.stormy.ui;
 
+import android.content.Intent;
+import android.os.Parcelable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.teamtreehouse.stormy.R;
+import com.teamtreehouse.stormy.weather.Hour;
+
+import java.util.Arrays;
 
 public class HourlyForecastActivity extends AppCompatActivity {
+
+    private Hour[] mHours;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hourly_forecast);
+
+        Intent intent = getIntent();
+        Parcelable[] parcelables = intent.getParcelableArrayExtra(MainActivity.HOURLY_FORECAST);
+        mHours = Arrays.copyOf(parcelables, parcelables.length, Hour[].class);
     }
 }
